6ca71a49df7c6beb5e3b706a7e3095cf7e78e89a
steadfasterX <steadfasterX@gmail.com>
2019-11-08 16:17:15 +0100

    g4: new place for AAPT (Screen Desnity) definitions

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,7 @@
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
+
 # Unless required by applicable law or agreed to in 
 # writing, software distributed under the License is 
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
@@ -17,7 +18,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_h815.mk
 
-COMMON_LUNCH_CHOICES := \
-    aosp_h815-userdebug \
-    aosp_h815-eng \
-    aosp_h815-user
+# Screen density
+# must be defined here as they get readonly later on
+PRODUCT_AAPT_PREF_CONFIG := 560dpi
+PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
